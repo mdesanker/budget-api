b34d65feaa29e22896be6db2e3b3b9573c40220e
Michael Desanker <mdesanker@gmail.com>
2022-02-18 09:58:27 -0500

    Add tests for update user route

--- routes/__test__/user.test.ts
+++ routes/__test__/user.test.ts
@@ -40,3 +40,95 @@ describe("GET /user/detail", () => {
     expect(res.body).not.toHaveProperty("password");
   });
 });
+
+// PUT ROUTES
+describe("PUT /user/update", () => {
+  it("return updated expenses user", async () => {
+    const res = await request(app)
+      .put("/user/update")
+      .send({
+        income: [
+          {
+            name: "Job",
+            amount: "65000",
+          },
+        ],
+        housing: [
+          {
+            name: "rent",
+            amount: "700",
+          },
+        ],
+        food: [
+          {
+            name: "groceries",
+            amount: "500",
+          },
+          {
+            name: "dining",
+            amount: "100",
+          },
+        ],
+        utilities: [
+          {
+            name: "electricity",
+            amount: "200",
+          },
+          {
+            name: "gas",
+            amount: "45",
+          },
+        ],
+        healthcare: [
+          {
+            name: "HSA",
+            amount: "15",
+          },
+        ],
+        loans: [
+          {
+            name: "car",
+            amount: "300",
+          },
+        ],
+        subscriptions: [
+          {
+            name: "amazon",
+            amount: "120",
+          },
+          {
+            name: "cell",
+            amount: "45",
+          },
+          {
+            name: "internet",
+            amount: "55",
+          },
+        ],
+      })
+      .set("x-auth-token", janeToken);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.income[0].amount).toEqual(65000);
+    expect(res.body.subscriptions.length).toEqual(3);
+  });
+
+  it("clear expense details with empty arrays", async () => {
+    const res = await request(app)
+      .put("/user/update")
+      .send({
+        income: [],
+        housing: [],
+        food: [],
+        utilities: [],
+        healthcare: [],
+        loans: [],
+        subscriptions: [],
+      })
+      .set("x-auth-token", janeToken);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.income).toEqual([]);
+    expect(res.body.subscriptions.length).toEqual(0);
+  });
+});
