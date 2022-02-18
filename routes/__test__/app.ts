f22f3a29e190e59d5d39093c0f4644d1619ba851
Michael Desanker <mdesanker@gmail.com>
2022-02-17 20:30:05 -0500

    Setup test file for auth routes

--- routes/__test__/app.ts
+++ routes/__test__/app.ts
@@ -5,4 +5,4 @@ const app = express();
 
 app.use("/auth", authRouter);
 
-export default app;
+export = app;
--- routes/__test__/auth.test.ts
+++ routes/__test__/auth.test.ts
@@ -1,9 +1,24 @@
-import request from "supertest";
-
 import app from "./app";
+import request from "supertest";
+import initializeTestServer from "../../config/mongoTestConfig";
+import mongoose from "mongoose";
 
 // GLOBAL VARIABLES
 
-// BEFORE TESTS
+// TEST SETUP
+beforeAll(async () => {
+  await initializeTestServer();
+});
 
-// AFTER TESTS
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+// POST ROUTES
+describe("POST /auth/register", () => {
+  it("return new user", async () => {
+    const res = await request(app).get("/auth/");
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
