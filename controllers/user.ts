7d12e46d05784c4c3a6eaf615744bf250c4c064e
Michael Desanker <mdesanker@gmail.com>
2022-02-18 09:42:54 -0500

    Add get user detail route

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import User from "../models/User";
 
 const user = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.send("GET user test");
+    // Get user
+    const user = await User.findById(req.user.id).select("-password");
+
+    res.json(user);
   } catch (err: unknown) {
     if (err instanceof Error) {
       res.status(500).send("Server error");
