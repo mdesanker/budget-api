2b3a9c7116f5ed3fa864cf810dbb4026ebc69524
Michael Desanker <mdesanker@gmail.com>
2022-02-18 11:09:17 -0500

    Add expenses model

---
+++ models/Expenses.ts
@@ -0,0 +1,63 @@
+import { Schema, Types, model } from "mongoose";
+
+export interface IExpenses {
+  _id?: string;
+  user: Types.ObjectId;
+  date: Date;
+  income?: { name: string; amount: number }[];
+  housing?: { name: string; amount: number }[];
+  food?: { name: string; amount: number }[];
+  utilities?: { name: string; amount: number }[];
+  healthcare?: { name: string; amount: number }[];
+  loans?: { name: string; amount: number }[];
+  subscriptions?: { name: string; amount: number }[];
+}
+
+const ExpensesSchema = new Schema<IExpenses>({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: Date, default: Date.now },
+  income: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  housing: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  food: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  utilities: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  healthcare: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  loans: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+  subscriptions: [
+    {
+      name: { type: String },
+      amount: { type: Number },
+    },
+  ],
+});
+
+export default model("Expenses", ExpensesSchema);
--- models/User.ts
+++ models/User.ts
@@ -2,17 +2,10 @@ import { Schema, model } from "mongoose";
 
 export interface IUser {
   _id?: string;
-  name: Record<string, string>;
+  name: { firstName: string; lastName: string };
   email: string;
   password: string;
   date?: Date;
-  income?: Record<string, any>[];
-  housing?: Record<string, any>[];
-  food?: Record<string, any>[];
-  utilities?: Record<string, any>[];
-  healthcare?: Record<string, any>[];
-  loans?: Record<string, any>[];
-  subscriptions?: Record<string, any>[];
 }
 
 const UserSchema = new Schema<IUser>({
@@ -23,48 +16,6 @@ const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   date: { type: Date, default: Date.now },
-  income: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  housing: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  food: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  utilities: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  healthcare: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  loans: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
-  subscriptions: [
-    {
-      name: { type: String },
-      amount: { type: Number },
-    },
-  ],
 });
 
 export default model("User", UserSchema);
